7d1ad34cdf5d7fed432654cbc313757a37044426
Tom Elmore <tom.elmore@easyroommate.com>
2015-03-18 12:38:53 +0000

    tested adding stuff to a stream

--- src/Scheduler.js
+++ src/Scheduler.js
@@ -1,8 +1,103 @@
 // Scheduler.js - Tom Elmore 2014
 
+var Stream = function(arr) {
+	
+	var _observers = []
+	
+	var _notifyObservers = function() {
+		
+		for(var i=0; i<_observers.length; i++) {
+			
+			_observers[i]();
+		}
+	};
+	
+	return {
+		
+		length : arr.length,
+		
+		shift : function() {
+			
+			var retVal = arr.shift();
+			
+			this.length = arr.length;
+			
+			_notifyObservers();
+			
+			return retVal;
+		},
+		
+		push : function(val) {
+			
+			var retVal = arr.push(val);
+			
+			this.length = arr.length;
+			
+			_notifyObservers();
+			
+			return retVal;
+		},
+		
+		observe : function(f) {
+			
+			_observers.push(f);
+		}
+	};
+};
 
-var Sheduler = function() {
+var TimedTask = function(arr) {
+		
+	var _freq;
+	var _action;
+	var _timer;
 	
+	this.every = function(milliseconds) {
+		
+		_freq = milliseconds;
+		
+		return this;
+	};
 	
+	this.act = function(action) {
+		
+		_action = action;
+		
+		return this;
+	};
+	
+	this.run = function() {
+		
+		_timer = setInterval(function() {
+			
+			if(arr.length > 0) {
+				
+				_action(arr.shift());
+			}
+			
+		}, _freq);
+		
+		return this;
+	};
+	
+	this.stop = function() {
+		
+		if(_timer != null) {
+			
+			clearInterval(_timer);
+		}
+	};
+};
+
+var Scheduler = {
+				
+	emit : function(arr) {
+		
+		return new TimedTask(arr);
+	},
+	
+	stream : function(arr) {
+			
+		return new Stream(arr);
+	}
 };
 
--- test/spec/SchedulerSpec.js
+++ test/spec/SchedulerSpec.js
@@ -5,11 +5,70 @@ describe("Scheduler", function() {
 	
 	it("exists", function(){
 
-		var scheduler = new Sheduler();
-		
-		expect(scheduler).not.toBeNull();
+		expect(Scheduler).not.toBeNull();
 	});
 
+	it("emits an array of values one at a time", function(done) {
+		
+		var expected = [1, 2, 3, 4];
+		var vals = [1, 2, 3, 4];
+		
+		var len = vals.length;
+		
+		var i = 0;
+				
+		var task = Scheduler.emit(vals).every(100).act(function(el) {
+						
+			if(i >= len-1) {
+				
+				task.stop();
+				
+				done();
+				
+				return;
+			}
+
+			expect(el).toBe(expected[i]);
+			
+			i++;
+		});
+		
+		task.run();
+	});
 	
+	it("accepts an observable array", function(done) {
+		
+		var expected = [1, 2, 3, 4];
+		var vals = [1, 2, 3, 4];
+		
+		var stream = Scheduler.stream([]);
+		
+		var len = vals.length;
+		
+		var i = 0;
+				
+		var task = Scheduler.emit(stream).every(100).act(function(el) {
+						
+			if(i >= len-1) {
+				
+				task.stop();
+				
+				done();
+				
+				return;
+			}
+
+			expect(el).toBe(expected[i]);
+			
+			i++;
+		});
+		
+		task.run();
+		
+		for(var j = 0; j<=expected.length; j++) {
+			
+			stream.push(expected[j]);
+		}
+	});
 });
 
